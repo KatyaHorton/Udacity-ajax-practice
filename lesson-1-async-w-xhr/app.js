39799887563a07a8d7775534eb1020155cbaf69c
Katya Horton <ekaterina.horton@gmail.com>
2018-07-02 14:31:17 +0100

    comments added to js file

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -4,25 +4,49 @@
     let searchedForText = 'Android';
     const responseContainer = document.querySelector('#response-container');
 
+// add event listener when submit button is clicked 
   form.addEventListener('submit', function (e) {
+	
+//prevents defaul action 
         e.preventDefault();
+	
+// sets response container to an empty string 
         responseContainer.innerHTML = '';
+	  
+//sets the value of the searchForText equal to what was typed in the searchField 
         searchedForText = searchField.value;
        
-      //We should create here our XHR object and send it to the server.
-      //We need to get the API keys so we can make this request as unlash needs authentication
-      //So we need first get the Access Key from https://unplash.com .
+//We should create here our XHR object and send it to the server.
+//We need to get the API keys so we can make this request as unlash needs authentication
+//first get the Access Key from https://unplash.com 
+
+// constructs a new XHR object called unsplashRequest
         const unsplashRequest = new XMLHttpRequest();
+
+//uses .open(method, url) method to initialize a newly-created request 
+//uses GET request to retrieve data
+	    unsplashRequest.open('GET', 
+
+//url includes searchForText variable, and will change depending on the user's request 							 
+		`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+	  
+	  
+//sets onload property to handle the successful responce of an XHR request
         unsplashRequest.onload = addImage;
-        unsplashRequest.onerror = function (err) {
+	  
+        unsplashRequest.onerror = function () {
             requestError(err, 'img');
 		
         };
-        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-        unsplashRequest.setRequestHeader('Authorization', 'Client-ID 0d0ecd17dfff39987d850edd198ce9aaa36b8c1ef027978b658796d7d93c79e0');
+// !!! setRequestheader must be called after .open and before .send
+        unsplashRequest.setRequestHeader('Authorization', 
+'Client-ID 0d0ecd17dfff39987d850edd198ce9aaa36b8c1ef027978b658796d7d93c79e0');
+
+// actually sends the request 
         unsplashRequest.send();
   });
   
+
      function addImage() {
         let htmlContent = '';
         const data = JSON.parse(this.responseText);
@@ -34,6 +58,7 @@
         </figure>`;
 
         responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+		 
         // let unplashImg = document.querySelector('.unplash_image');
         // unplashImg.setAttribute('src', firstImage.urls.regular);
 
